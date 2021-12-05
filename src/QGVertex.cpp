2ff6c7b2d65993889aa50e9ab1228e6d24e276f6
Mike <wildstyl3r@yandex.ru>
2021-12-05 20:49:08 +0500

    vertex' QPainterPath now changes on highlighting

--- src/QGVertex.cpp
+++ src/QGVertex.cpp
@@ -8,7 +8,17 @@ void QG::Vertex::setPath(const QPainterPath &path) { _path = path; }
 void QG::Vertex::setToolTip(const QString &toolTip) { _tooltip = toolTip; }
 void QG::Vertex::setPosition(const QPointF &position) {_position = position; }
 void QG::Vertex::setColor(const QColor &color) { _color = color; }
-void QG::Vertex::highlight(bool h) { _highlighted = h; }
+void QG::Vertex::highlight(bool h) {
+    _highlighted = h;
+    if (h){
+        _path.clear();
+        _path.addEllipse(QPointF(), Vertex::highlightedRadius, Vertex::highlightedRadius);
+    } else {
+        _path.clear();
+        _path.addEllipse(QPointF(), Vertex::defaultRadius, Vertex::defaultRadius);
+    }
+
+}
 
 QPainterPath QG::Vertex::path() const { return _path; }
 QPointF QG::Vertex::position() const { return _position; }
